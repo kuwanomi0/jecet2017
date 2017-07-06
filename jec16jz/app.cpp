c1115748c7ba46d82d1c761d3e1798d7032d5f98
kuwanomi0 <kuwanomi0@live.jp>
2017-07-06 21:00:38 +0900

    courseクラスの追加　L、R別に区間ごとに値ができるようにapp.cppのグローバル領域に追加を行いました

--- jec16jz/app.cpp
+++ jec16jz/app.cpp
@@ -21,6 +21,8 @@
 #include "PID.h"
 #include "Distance.h"
 
+#include "Course.h"
+
 using namespace ev3api;
 
 #define DEBUG
@@ -40,7 +42,7 @@ static FILE     *bt = NULL;      /* Bluetoothファイルハンドル */
 #define GYRO_OFFSET           0  /* ジャイロセンサオフセット値(角速度0[deg/sec]時) */
 #define RGB_WHITE           500  /* 白色のRGBセンサの合計 */
 #define RGB_BLACK            20  /* 黒色のRGBセンサの合計 */
-#define RGB_TARGET          250  /* 中央の境界線のRGBセンサ合計値 */
+#define RGB_TARGET          260  /* 中央の境界線のRGBセンサ合計値 */
 #define RGB_NULL              5  /* 何もないときのセンサの合計 */
 #define KP_WALK         0.1200F  /* 走行用定数P */
 #define KI_WALK         0.0000F  /* 走行用定数I */
@@ -52,8 +54,10 @@ static FILE     *bt = NULL;      /* Bluetoothファイルハンドル */
 
 /* 尻尾に関するマクロ */
 #define TAIL_ANGLE_STAND_UP   94 /* 完全停止時の角度[度] */
+#define TAIL_ANGLE_ROKET      99 /* 完全停止時の角度[度] */
 #define TAIL_ANGLE_DRIVE       3 /* バランス走行時の角度[度] */
-#define KP_TAIL            2.00F /* 尻尾用定数P */
+#define TAIL_ANGLE_STOP       80 /* バランス走行時の角度[度] */
+#define KP_TAIL            1.00F /* 尻尾用定数P */
 #define KI_TAIL            0.01F /* 尻尾用定数I */
 #define KD_TAIL            0.15F /* 尻尾用定数D */
 #define PWM_ABS_MAX           60 /* 完全停止用モータ制御PWM絶対最大値 */
@@ -87,6 +91,27 @@ Balancer balancer;              // <1>
 PID pid_walk(KP_WALK, KI_WALK, KD_WALK); /* 走行用のPIDインスタンス */
 PID pid_tail(KP_TAIL, KI_TAIL, KD_TAIL); /* 尻尾用のPIDインスタンス */
 Distance distance_way;
+static Course gCourse[]  {
+    { 0,     0,100, 0.0000F, 0.0000F, 0.0000F },  // 左旋回1.25秒
+    { 0,    10, 85, 0.0600F, 0.0000F, 0.0000F },  // 左旋回1.25秒
+    { 1,  2221,100, 0.1000F, 0.0000F, 0.0010F },  // 直進5秒
+    { 2,  3641, 85, 0.1000F, 0.0000F, 0.1000F },  // 直進5秒
+    { 3,  5197, 85, 0.1200F, 0.0000F, 0.1000F },  // 左旋回1.25秒
+    { 4,  6786, 85, 0.1200F, 0.0000F, 0.1000F },   // 直進2.5秒
+    { 5,  8400, 85, 0.0600F, 0.0000F, 0.0000F },   // 直進2.5秒
+    { 6, 10031,100, 0.1200F, 0.0000F, 0.1000F },   // 直進2.5秒
+    { 7, 99999,  1, 0.1200F, 0.0000F, 0.1000F },   // 直進2.5秒
+
+    { 0,     0,100, 0.0000F, 0.0000F, 0.0000F },  // 左旋回1.25秒
+    { 0,    10, 85, 0.0600F, 0.0000F, 0.0000F },  // 左旋回1.25秒
+    { 1,  2251,100, 0.1000F, 0.0000F, 0.0010F },  // 直進5秒
+    { 2,  5383, 85, 0.1000F, 0.0000F, 0.1000F },  // 直進5秒
+    { 3,  6298, 85, 0.1200F, 0.0000F, 0.1000F },  // 左旋回1.25秒
+    { 5,  8600, 85, 0.1200F, 0.0000F, 0.1000F },   // 直進2.5秒
+    { 4,  7228, 85, 0.0600F, 0.0000F, 0.0000F },   // 直進2.5秒
+    { 6, 10298,100, 0.1200F, 0.0000F, 0.1000F },   // 直進2.5秒
+    { 7, 99999,  1, 0.1200F, 0.0000F, 0.1000F },   // 直進2.5秒
+};
 
 /* メインタスク */
 void main_task(intptr_t unused)
@@ -95,7 +120,10 @@ void main_task(intptr_t unused)
     int8_t    turn;         /* 旋回命令 */
     int8_t    pwm_L, pwm_R; /* 左右モータPWM出力 */
     rgb_raw_t rgb_level;    /* カラーセンサーから取得した値を格納する構造体 */
-    // int32_t   glay = 0;     /* TODO 01: glay検出用テスト */
+    int8_t course_number = 0;
+    int count = 0;
+    int roket = 0;
+    int tail_i = 0;
 
     /* 各オブジェクトを生成・初期化する */
     touchSensor = new TouchSensor(PORT_1);
@@ -132,6 +160,11 @@ void main_task(intptr_t unused)
         {
             break; /* リモートスタート */
         }
+        if (bt_cmd == 2)
+        {
+            count = 9;
+            break; /* リモートスタート */
+        }
 
         if (touchSensor->isPressed())
         {
@@ -163,9 +196,20 @@ void main_task(intptr_t unused)
             break;
         }
 
-        ev3_led_set_color(LED_RED);
-        tail_control(TAIL_ANGLE_DRIVE); /* バランス走行用角度に制御 */
+        if((course_number % 2) == 1) {
+            ev3_led_set_color(LED_RED);
+        }
+        else {
+            ev3_led_set_color(LED_GREEN);
+        }
+        if (bt_cmd == 6) {
 
+        }
+        else if(roket++ < 400)
+            tail_control(TAIL_ANGLE_ROKET); /* バランス走行用角度に制御 */
+        else {
+            tail_control(TAIL_ANGLE_DRIVE); /* バランス走行用角度に制御 */
+        }
         colorSensor->getRawColor(rgb_level); /* RGB取得 */
 
         /* 転倒時の停止処理 */
@@ -173,35 +217,31 @@ void main_task(intptr_t unused)
             break;
         }
 
-    //     if ( 90 <= rgb_level.b &&                             /* TODO 01: glay検出用*/
-    //         rgb_level.b <= 150 &&                             /* TODO 01: glay検出用*/
-    //         rgb_level.b * 2 > (rgb_level.r + rgb_level.g)) {  /* TODO 01: glay検出用*/
-    //         glay++;                                           /* TODO 01: glay検出用*/
-    //     }                                                     /* TODO 01: glay検出用*/
-    //     else {                                                /* TODO 01: glay検出用*/
-    //         glay = 0;                                         /* TODO 01: glay検出用*/
-    //     }                                                     /* TODO 01: glay検出用*/
     distance_now = distance_way.distanceAll(leftMotor->getCount(), rightMotor->getCount());
-    if (distance_now <= 20000) { /* TODO 02: 距離確認用 */
+    if (distance_now >= gCourse[count].getDis()) {
+        course_number = gCourse[count].getCourse_num();
+        forward = gCourse[count].getForward();
+        pid_walk.setPID(gCourse[count].getP(), gCourse[count].getI(), gCourse[count].getD());
+        count++;
+    }
+
         if (sonar_alert() == 1) {/* 障害物検知 */
             forward = turn = 0; /* 障害物を検知したら停止 */
             ev3_led_set_color(LED_RED);
         }
-        // else if (glay >= 30) { /* TODO 01: glay検出用*/
-        //     turn = 13;         /* TODO 01: glay検出用*/
-        // }                      /* TODO 01: glay検出用*/
         else
         {
-            forward = FORWARD_K; /* 前進命令 */
+            if (bt_cmd == 7 || bt_cmd ==6)
+            {
+                forward = -20;
+            }
+            else {
+                forward = gCourse[count].getForward(); /* 前進命令 */
+            }
             /* PID制御 */
             // turn =  pid_walk.calcControl(((RGB_BLACK + RGB_WHITE) / 2) - (rgb_level.r + rgb_level.g + rgb_level.b));
             turn =  pid_walk.calcControl(RGB_TARGET - (rgb_level.r + rgb_level.g + rgb_level.b));
         }
-    }/* TODO 02: 距離確認用 */
-    else {/* TODO 02: 距離確認用 */
-        forward = 100;/* TODO 02: 距離確認用 */
-        turn = 70;/* TODO 02: 距離確認用 */
-    }/* TODO 02: 距離確認用 */
 
         /* 倒立振子制御API に渡すパラメータを取得する */
         motor_ang_l = leftMotor->getCount();
@@ -218,7 +258,23 @@ void main_task(intptr_t unused)
 
         leftMotor->setPWM(pwm_L);
         rightMotor->setPWM(pwm_R);
-        syslog(LOG_NOTICE, "DEBUG, DIS:%5d, GYRO:%3d, R:%3d, G:%3d, B:%3d, T:%3d\r", distance_now, gyro, rgb_level.r, rgb_level.g, rgb_level.b, (rgb_level.r + rgb_level.g + rgb_level.b));
+        // syslog(LOG_NOTICE, "DEBUG, DIS:%5d, GYRO:%3d, R:%3d, G:%3d, B:%3d, T:%4d\r", distance_now, gyro, rgb_level.r, rgb_level.g, rgb_level.b, (rgb_level.r + rgb_level.g + rgb_level.b));
+        syslog(LOG_NOTICE, "DEBUG, DIS:%5d, GYRO:%3d, C:%2d, F:%3d\r", distance_now, gyro, course_number, forward);
+        // if (bt_cmd == 1)
+        // {
+        //     syslog(LOG_NOTICE, "DEBUG, DIS:%5d, GYRO:%3d, C:%2d, F:%3d\r", distance_now, gyro, course_number, forward);
+        //     bt_cmd = 0;
+        // }
+        if (bt_cmd == 6)
+        {
+            if (tail_i++ < 400) {
+                tail_control(TAIL_ANGLE_STOP);
+            }
+            else {
+                break;
+            }
+        }
+
 
         clock->sleep(4); /* 4msec周期起動 */
     }
@@ -304,6 +360,15 @@ void bt_task(intptr_t unused)
         {
         case '1':
             bt_cmd = 1;
+            break;
+        case '2':
+            bt_cmd = 2;
+            break;
+        case '6':
+            bt_cmd = 6;
+            break;
+        case '7':
+            bt_cmd = 7;
             break;
         default:
             break;
