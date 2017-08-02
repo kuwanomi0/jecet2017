67b4439330d98ba1c362afc61bf4d710175f08f6
kuwanomi0 <kuwanomi0@live.jp>
2017-08-02 11:53:11 +0900

    光センサーにLPF（ローパスフィルタ）を追加 PID値を調整して高速な走行ができるようになりました

--- jec16jz/app.cpp
+++ jec16jz/app.cpp
@@ -43,12 +43,12 @@ static FILE     *bt = NULL;      /* Bluetoothファイルハンドル */
 #define RGB_WHITE           500  /* 白色のRGBセンサの合計 */
 #define RGB_BLACK            20  /* 黒色のRGBセンサの合計 */
 #define RGB_TARGET          240  /* 中央の境界線のRGBセンサ合計値 */
-#define RGB_NULL              5  /* 何もないときのセンサの合計 */
-#define KP_WALK         0.1200F  /* 走行用定数P TODO :1 この値は走行時には使われていない*/
-#define KI_WALK         0.0000F  /* 走行用定数I TODO :1 この値は走行時には使われていない*/
-#define KD_WALK         0.1000F  /* 走行用定数D TODO :1 この値は走行時には使われていない*/
-#define FORWARD_K            75  /* ロボットの前進速度 TODO :1 この値は走行時には使われていない*/
+#define RGB_NULL              7  /* 何もないときのセンサの合計 */
+#define KP_WALK         0.1200F  /* コースを利用しない際の初期の係数P */
+#define KI_WALK         0.0001F  /* コースを利用しない際の初期の係数I */
+#define KD_WALK         1.0000F  /* コースを利用しない際の初期の係数D */
 #define PIDX                  1  /* PID倍率 */
+#define KLP                 0.8  /* LPF用係数*/
 
 /* 超音波センサーに関するマクロ */
 #define SONAR_ALERT_DISTANCE 20  /* 超音波センサによる障害物検知距離[cm] */
@@ -60,7 +60,7 @@ static FILE     *bt = NULL;      /* Bluetoothファイルハンドル */
 #define TAIL_ANGLE_STOP       75 /* 停止処理時の角度[度] */
 #define KP_TAIL            1.00F /* 尻尾用定数P */
 #define KI_TAIL            0.01F /* 尻尾用定数I */
-#define KD_TAIL            0.15F /* 尻尾用定数D */
+#define KD_TAIL            1.0F /* 尻尾用定数D */
 #define PWM_ABS_MAX           60 /* 完全停止用モータ制御PWM絶対最大値 */
 
 /* LCDフォントサイズ */
@@ -89,60 +89,37 @@ PID pid_walk(KP_WALK, KI_WALK, KD_WALK); /* 走行用のPIDインスタンス */
 PID pid_tail(KP_TAIL, KI_TAIL, KD_TAIL); /* 尻尾用のPIDインスタンス */
 Distance distance_way;
 
-#define R_COURSE   15   /* Rコースの一番最初の値が格納されている番号、Lコースの配列の追加削除をした際にはRコースの先頭配列の番号を変える必要があります */  //TODO :2 コース関連 だいぶ改善されました
-
-static Course gCourseL[]  {   //TODO :2 コース関連 だいぶ改善されました
-    /* Lコース用配列 */
-    { 0,     0,110,  0, 0.0300F, 0.00000F, 0.0050F },  // 直線 //TODO :2 コース関連 だいぶ改善されました
-    { 1,  2225,110,-13, 0.0500F, 0.00000F, 0.0500F },  // 曲線 //TODO :2 コース関連 だいぶ改善されました
-    { 2,  3850,110,  8, 0.0800F, 0.00000F, 0.0530F },  // 曲線 //TODO :2 コース関連 だいぶ改善されました
-    { 3,  4554,110,  2, 0.0700F, 0.00000F, 0.0530F },   //TODO :2 コース関連 だいぶ改善されました
-    { 4,  5209, 75,  0, 0.1000F, 0.00000F, 0.1030F },   //TODO :2 コース関連 だいぶ改善されました
-    { 5,  6134, 75,  0, 0.0900F, 0.00000F, 0.0530F },   //TODO :2 コース関連 だいぶ改善されました
-    { 6,  6674, 75,  0, 0.1200F, 0.00000F, 0.0930F },   //TODO :2 コース関連 だいぶ改善されました
-    { 7,  7562, 75,  0, 0.1000F, 0.00000F, 0.1030F },   //TODO :2 コース関連 だいぶ改善されました
-    { 8,  8612, 75,  0, 0.0600F, 0.00000F, 0.0330F },   //TODO :2 コース関連 だいぶ改善されました
-    { 9,  9900, 75,  0, 0.0900F, 0.00000F, 0.0330F },   //TODO :2 コース関連 だいぶ改善されました
-    {10, 10030, 75,  0, 0.0000F, 0.00000F, 0.0030F },   //TODO :2 コース関連 だいぶ改善されました
-    {11, 10401, 75,  0, 0.1200F, 0.00000F, 0.1030F },   //TODO :2 コース関連 だいぶ改善されました
-    {12, 11576, 75,  0, 0.0000F, 0.00000F, 0.0030F },   //TODO :2 コース関連 だいぶ改善されました
-    {13, 11766, 75,  0, 0.1200F, 0.00000F, 0.1030F },   //TODO :2 コース関連 だいぶ改善されました
-    {14, 99999, 75,  0, 0.0000F, 0.00000F, 0.00306F}    //TODO :2 コース関連 だいぶ改善されました
+static Course gCourseL[] {  // TODO 2: コース関連 だいぶ改善されました これで30.36secでた。
+    { 0,     0,122,  0, 0.0500F, 0.0000F, 1.0000F }, //スタート
+    { 1,  2000,106,  0, 0.1200F, 0.0002F, 0.7000F }, //大きく右
+    { 2,  3927,109,  0, 0.1150F, 0.0002F, 0.5000F }, //左
+    { 3,  4754,121,  0, 0.0700F, 0.0000F, 1.0000F }, //直
+    { 4,  5209,109,  0, 0.1150F, 0.0002F, 0.5000F }, //左
+    { 5,  6134,122,  0, 0.0800F, 0.0000F, 1.0000F }, //直
+    { 6,  6674,106,  0, 0.1300F, 0.0002F, 1.2000F }, //左
+    { 7,  7562,107,  0, 0.1100F, 0.0002F, 0.5000F }, //右
+    { 8,  8800,122,  0, 0.0450F, 0.0000F, 1.0000F }, //直GOOLまで
+    {10, 10030,122,  0, 0.0000F, 0.0000F, 0.0000F }, //灰
+    {11, 10351,106,  0, 0.1150F, 0.0002F, 0.5000F }, //左
+    {12, 11576, 71,  0, 0.0000F, 0.0000F, 0.0000F }, //灰
+    {13, 11766,121,  0, 0.0500F, 0.0000F, 1.0000F }, //直
+    {14, 99999,  1,  0, 0.0000F, 0.0000F, 0.0000F }  //終わりのダミー
 };
 
-static Course gCourseLFullPID[] {
-    { 0,     0,125,  0, 0.0600F, 0.0000F, 0.0000F },   //TODO :2 コース関連 だいぶ改善されました
-    { 1,  2100, 85,  0, 0.1000F, 0.0000F, 0.0100F },   //TODO :2 コース関連 だいぶ改善されました
-    { 2,  3927, 85,  0, 0.1000F, 0.0000F, 0.1000F },   //TODO :2 コース関連 だいぶ改善されました
-    { 3,  4754,100,  0, 0.0700F, 0.0000F, 0.0500F },   //TODO :2 コース関連 だいぶ改善されました
-    { 4,  5209, 85,  0, 0.1200F, 0.0000F, 0.1000F },   //TODO :2 コース関連 だいぶ改善されました
-    { 5,  6134,100,  0, 0.0800F, 0.0000F, 0.0500F },   //TODO :2 コース関連 だいぶ改善されました
-    { 6,  6674, 85,  0, 0.1200F, 0.0000F, 0.1000F },   //TODO :2 コース関連 だいぶ改善されました
-    { 7,  7562, 85,  0, 0.1200F, 0.0000F, 0.1000F },   //TODO :2 コース関連 だいぶ改善されました
-    { 8,  8612,100,  0, 0.0600F, 0.0000F, 0.0300F },   //TODO :2 コース関連 だいぶ改善されました
-    { 9,  9900, 30,  0, 0.0900F, 0.0000F, 0.0300F },   //TODO :2 コース関連 だいぶ改善されました
-    {10, 10030, 30,  0, 0.0000F, 0.0000F, 0.0000F },   //TODO :2 コース関連 だいぶ改善されました
-    {11, 10351, 30,  0, 0.1200F, 0.0000F, 0.1000F },   //TODO :2 コース関連 だいぶ改善されました
-    {12, 11576, 30,  0, 0.0000F, 0.0000F, 0.0000F },   //TODO :2 コース関連 だいぶ改善されました
-    {13, 11766, 50,  0, 0.1200F, 0.0000F, 0.1000F },   //TODO :2 コース関連 だいぶ改善されました
-    {14, 99999,  1,  0, 0.0000F, 0.0000F, 0.0000F },   //TODO :2 コース関連 だいぶ改善されました
+static Course gCourseR[]  {  //TODO :2 コース関連 だいぶ改善されました　31.25secでた
+    { 0,     0,122,  0, 0.0500F, 0.0000F, 1.0000F }, //スタート
+    { 1,  2200,106,  0, 0.1200F, 0.0002F, 0.4900F }, //大きく右
+    { 1,  3700,106,  0, 0.1000F, 0.0001F, 0.5100F }, //大きく右
+    { 2,  5400,108,  0, 0.1200F, 0.0001F, 0.8000F }, //左やや直進
+    { 3,  6350,103,  0, 0.1260F, 0.0002F, 0.4500F }, //強く左
+    { 4,  7150,106,  0, 0.1100F, 0.0002F, 0.5000F }, //緩やかに大きく右
+    { 5,  8750,122,  0, 0.0500F, 0.0000F, 1.0000F }, //直GOOLまで
+    { 6, 10300,122,  0, 0.0000F, 0.0000F, 0.0000F }, //直GOOLまで
+    { 7, 10530,106,  0, 0.1200F, 0.0002F, 0.5000F }, //左
+    { 8, 11750,106,  0, 0.0000F, 0.0000F, 0.0000F }, //灰
+    { 9, 12167,106,  0, 0.1200F, 0.0002F, 0.6000F }, //直?
+    {10, 99999,  1,  0, 0.0000F, 0.0000F, 0.0000F }  //終わりのダミー
 };
-
-static Course gCourseR[]  {
-    /* Rコース用配列 */
-    { 0,     0,125,  0, 0.0600F, 0.0000F, 0.0100F },   //TODO :2 コース関連 だいぶ改善されました
-    { 1,  2225, 80,  0, 0.1000F, 0.0000F, 0.1000F },   //TODO :2 コース関連 だいぶ改善されました
-    { 2,  5400, 80,  0, 0.1000F, 0.0000F, 0.1000F },   //TODO :2 コース関連 だいぶ改善されました
-    { 3,  6350, 80,  0, 0.1200F, 0.0000F, 0.1000F },   //TODO :2 コース関連 だいぶ改善されました
-    { 4,  7250, 80,  0, 0.1000F, 0.0000F, 0.1000F },   //TODO :2 コース関連 だいぶ改善されました
-    { 5,  8900,100,  0, 0.0600F, 0.0000F, 0.0300F },   //TODO :2 コース関連 だいぶ改善されました
-    { 6, 10198, 30,  0, 0.0900F, 0.0000F, 0.0300F },   //TODO :2 コース関連 だいぶ改善されました
-    { 7, 10298, 30,  0, 0.0000F, 0.0000F, 0.0000F },   //TODO :2 コース関連 だいぶ改善されました
-    { 8, 10570, 20,  0, 0.1000F, 0.0000F, 0.1000F },   //TODO :2 コース関連 だいぶ改善されました
-    { 9, 11950, 50,  0, 0.0000F, 0.0000F, 0.0000F },   //TODO :2 コース関連 だいぶ改善されました
-    {10, 12167, 30,  0, 0.1200F, 0.0000F, 0.1000F },   //TODO :2 コース関連 だいぶ改善されました
-    {11, 99999,  1,  0, 0.0000F, 0.0000F, 0.0000F }   //TODO :2 コース関連 だいぶ改善されました
-};   //TODO :2 コース関連 だいぶ改善されました
 
 // サウンド
 #define NOTE_C4 (261.63)
@@ -157,17 +134,18 @@ static Course gCourseR[]  {
 /* メインタスク */
 void main_task(intptr_t unused)
 {
-    int8_t    forward;      /* 前後進命令 */
-    int8_t    turn;         /* 旋回命令 */
+    int8_t    forward = 50;      /* 前後進命令 */
+    int8_t    turn = 0;         /* 旋回命令 */
     int8_t    pwm_L, pwm_R; /* 左右モータPWM出力 */
     rgb_raw_t rgb_level;    /* カラーセンサーから取得した値を格納する構造体 */
-    int8_t course_number = 0; //TODO :2 強引な区間設定によって作られた変数です
-    int count = 0;  //TODO :2 強引な区間設定によって作られた変数です
+    int course_number = 0; //TODO :2 コース関連 だいぶ改善されました
+    int count = 0;  //TODO :2 コース関連 だいぶ改善されました
     int roket = 0;  //TODO :3 ロケットスタート用変数 タイマーの役割をしています
-    int tail_i = 0; //TODO :4 おまけ コマンドでの終了する際のタイマー用変数
-    int8_t forward_course = 50; //TODO :2 強引な区間設定によって作られた変数
-    int8_t turn_course = 0; //TODO :2
-    uint16_t rgb_total;
+    // int tail_i = 0; //TODO :4 おまけ コマンドでの終了する際のタイマー用変数
+    int forward_course = 50; //TODO :2 コース関連 だいぶ改善されました
+    int turn_course = 0; //TODO :2 コース関連 だいぶ改善されました
+    uint16_t rgb_total = RGB_TARGET;
+    uint16_t rgb_before;
     Course* mCourse = NULL;
 
     /* 各オブジェクトを生成・初期化する */
@@ -206,7 +184,7 @@ void main_task(intptr_t unused)
         tail_control(angle); /* 完全停止用角度に制御、調整も可 */
 
         /* Lコース */  //TODO :2 コース関連 だいぶ改善されました
-        if (bt_cmd == 1 || touchSensor->isPressed())
+        if (bt_cmd == 1 || touchSensor->isPressed()) //TODO :5 現在タッチセンサーでのスタートはLコースを走るメソッドを選びます
         {
             mCourse = gCourseL;
             break; /* リモートスタート */
@@ -217,16 +195,6 @@ void main_task(intptr_t unused)
             mCourse = gCourseR;//TODO :2 コース関連 だいぶ改善されました
             break; /* リモートスタート */
         }
-        if (bt_cmd == 3)
-        {
-            mCourse = gCourseLFullPID;//TODO :2 コース関連 だいぶ改善されました
-            break; /* リモートスタート */
-        }
-
-        // if (bt_cmd == '?')     // TODO :BAKA
-        // {     // TODO :BAKA
-            // break;     // TODO :BAKA
-        // }     // TODO :BAKA
 
         // if (touchSensor->isPressed())
         // {
@@ -347,7 +315,6 @@ void main_task(intptr_t unused)
     * Main loop for the self-balance control algorithm
     */
     while(1)
-    // while(bt_cmd != '?')   // TODO :BAKA   BAKAプログラム有効用条件式
     {
         int32_t motor_ang_l, motor_ang_r;
         int32_t gyro, volt;
@@ -375,9 +342,9 @@ void main_task(intptr_t unused)
             tail_control(TAIL_ANGLE_DRIVE); /* バランス走行用角度に制御 */
         }
 
+        rgb_before = rgb_total;
         colorSensor->getRawColor(rgb_level); /* RGB取得 */
-        rgb_total = (rgb_level.r + rgb_level.g + rgb_level.b);
-
+        rgb_total = (rgb_level.r + rgb_level.g + rgb_level.b)  * KLP + rgb_before * (1 - KLP); //ローパスフィルター
         /* 転倒時の停止処理 */
         if(rgb_total <= RGB_NULL) {
             break;
@@ -387,13 +354,13 @@ void main_task(intptr_t unused)
         distance_now = distance_way.distanceAll(leftMotor->getCount(), rightMotor->getCount());
 
         /* 区間変更を監視、行うプログラム */
-        if (distance_now >= mCourse[count].getDis()) {      //TODO :2 もっといい書き方があると思います。
-            course_number  = mCourse[count].getCourse_num();      //TODO :2 もっといい書き方があると思います。
-            forward_course = mCourse[count].getForward();      //TODO :2 もっといい書き方があると思います。
+        if (distance_now >= mCourse[count].getDis()) {      //TODO :2 コース関連 だいぶ改善されました
+            course_number  = mCourse[count].getCourse_num();      //TODO :2 コース関連 だいぶ改善されました
+            forward_course = mCourse[count].getForward();      //TODO :2 コース関連 だいぶ改善されました
             turn_course    = mCourse[count].getTurn();
-            pid_walk.setPID(mCourse[count].getP() * PIDX, mCourse[count].getI() * PIDX, mCourse[count].getD() * PIDX);      //TODO :2 もっといい書き方があると思います。
-            count++;      //TODO :2 もっといい書き方があると思います。
-        }      //TODO :2 もっといい書き方があると思います。
+            pid_walk.setPID(mCourse[count].getP() * PIDX, mCourse[count].getI() * PIDX, mCourse[count].getD() * PIDX);      //TODO :2 コース関連 だいぶ改善されました
+            count++;      //TODO :2 コース関連 だいぶ改善されました
+        }      //TODO :2 コース関連 だいぶ改善されました
 
         if (sonar_alert() == 1) {/* 障害物検知 */
             forward = turn = 0; /* 障害物を検知したら停止 */
@@ -435,11 +402,19 @@ void main_task(intptr_t unused)
 
         /* ログを送信する処理　*/
         // syslog(LOG_NOTICE, "DEBUG, C:%2d, DIS:%5d, GYRO:%3d, R:%3d, G:%3d, B:%3d, T:%4d\r", course_number, distance_now, gyro, rgb_level.r, rgb_level.g, rgb_level.b, rgb_total);
-        syslog(LOG_NOTICE, "D:%5d, G:%3d, T:%3d, L:%3d, R:%3d\r", distance_now, gyro, turn, pwm_L, pwm_R);
+        // syslog(LOG_NOTICE, "D:%5d, G:%3d, T:%3d, L:%3d, R:%3d\r", distance_now, gyro, turn, pwm_L, pwm_R);
+        syslog(LOG_NOTICE, "D:%5d, G:%3d, T:%3d, RGB%3d\r", distance_now, gyro, turn, rgb_total);
         // if (bt_cmd == 1)
         // {
         //     syslog(LOG_NOTICE, "DEBUG, DIS:%5d, GYRO:%3d, C:%2d, F:%3d\r", distance_now, gyro, course_number, forward);
         //     bt_cmd = 0;
+        // }
+
+        // if (course_number % 2 != 0) {
+        //     ev3_speaker_play_tone (100, 1000);
+        // }
+        // else {
+        //     ev3_speaker_play_tone (400, 1000);
         // }
 
         // TODO :4 おまけ
@@ -463,77 +438,6 @@ void main_task(intptr_t unused)
     leftMotor->reset();
     rightMotor->reset();
     tailMotor->reset();
-
-    /* ここからBAKAプログラム */
-    if (bt_cmd == '?') {
-        syslog(LOG_NOTICE, "music mode\r\r");
-        ev3_speaker_set_volume (1);
-        while (bt_cmd != '-') {
-            syslog(LOG_NOTICE, "play music!!\r");
-            if (bt_cmd == 'z') {
-                ev3_speaker_play_tone (NOTE_C5, 1000);
-                bt_cmd = 0;
-            }
-            if (bt_cmd == 's') {
-                ev3_speaker_play_tone (NOTE_CS5, 1000);
-                bt_cmd = 0;
-            }
-            if (bt_cmd == 'x') {
-                ev3_speaker_play_tone (NOTE_D5, 1000);
-                bt_cmd = 0;
-            }
-            if (bt_cmd == 'd') {
-                ev3_speaker_play_tone (NOTE_DS5, 1000);
-                bt_cmd = 0;
-            }
-            if (bt_cmd == 'c') {
-                ev3_speaker_play_tone (NOTE_E5, 1000);
-                bt_cmd = 0;
-            }
-            if (bt_cmd == 'v') {
-                ev3_speaker_play_tone (NOTE_F5, 1000);
-                bt_cmd = 0;
-            }
-            if (bt_cmd == 'g') {
-                ev3_speaker_play_tone (NOTE_FS5, 1000);
-                bt_cmd = 0;
-            }
-            if (bt_cmd == 'b') {
-                ev3_speaker_play_tone (NOTE_G5, 1000);
-                bt_cmd = 0;
-            }
-            if (bt_cmd == 'h') {
-                ev3_speaker_play_tone (NOTE_GS5, 1000);
-                bt_cmd = 0;
-            }
-            if (bt_cmd == 'n') {
-                ev3_speaker_play_tone (NOTE_A5, 1000);
-                bt_cmd = 0;
-            }
-            if (bt_cmd == 'j') {
-                ev3_speaker_play_tone (NOTE_AS5, 1000);
-                bt_cmd = 0;
-            }
-            if (bt_cmd == 'm') {
-                ev3_speaker_play_tone (NOTE_B5, 1000);
-                bt_cmd = 0;
-            }
-            if (bt_cmd == ',') {
-                ev3_speaker_play_tone (NOTE_C6, 1000);
-                bt_cmd = 0;
-            }
-            if (bt_cmd == 'l') {
-                ev3_speaker_play_tone (NOTE_CS6, 1000);
-                bt_cmd = 0;
-            }
-            if (bt_cmd == '.') {
-                ev3_speaker_play_tone (NOTE_D6, 1000);
-                bt_cmd = 0;
-            }
-            clock->sleep(50);
-        }
-    }
-    /* ここまでBAKAプログラム */
 
     ter_tsk(BT_TASK);
     fclose(bt);
@@ -613,7 +517,7 @@ void bt_task(intptr_t unused)
         switch(c)
         {
         case '1':
-        case 'l':                         // XXX :BAKA使用時にはコメントアウト
+        case 'l':
             bt_cmd = 1;
             break;
         case '2':
@@ -641,52 +545,6 @@ void bt_task(intptr_t unused)
         case ']':   // 下
             bt_cmd = ']';
             break;
-        // case '?':                                       // TODO :BAKA
-            // bt_cmd = '?';                                       // TODO :BAKA
-            // break;                                       // TODO :BAKA
-        // case '-':                                       // TODO :BAKA
-            // bt_cmd = '-';                                       // TODO :BAKA
-            // break;                                       // TODO :BAKA
-        // case 'z':                                       // TODO :BAKA
-            // bt_cmd = 'z';                                       // TODO :BAKA
-            // break;                                       // TODO :BAKA
-        // case 'x':                                       // TODO :BAKA
-            // bt_cmd = 'x';                                       // TODO :BAKA
-            // break;                                       // TODO :BAKA
-        // case 'c':                                       // TODO :BAKA
-            // bt_cmd = 'c';                                       // TODO :BAKA
-            // break;                                       // TODO :BAKA
-        // case 'v':                                       // TODO :BAKA
-            // bt_cmd = 'v';                                       // TODO :BAKA
-            // break;                                       // TODO :BAKA
-        // case 'g':                                       // TODO :BAKA
-            // bt_cmd = 'g';                                       // TODO :BAKA
-            // break;                                       // TODO :BAKA
-        // case 'b':                                       // TODO :BAKA
-            // bt_cmd = 'b';                                       // TODO :BAKA
-            // break;                                       // TODO :BAKA
-        // case 'h':                                       // TODO :BAKA
-            // bt_cmd = 'h';                                       // TODO :BAKA
-            // break;                                       // TODO :BAKA
-        // case 'n':                                       // TODO :BAKA
-            // bt_cmd = 'n';                                       // TODO :BAKA
-            // break;                                       // TODO :BAKA
-        // case 'j':                                       // TODO :BAKA
-            // bt_cmd = 'j';                                       // TODO :BAKA
-            // break;                                       // TODO :BAKA
-        // case 'm':                                       // TODO :BAKA
-            // bt_cmd = 'm';                                       // TODO :BAKA
-            // break;                                       // TODO :BAKA
-        // case ',':                                       // TODO :BAKA
-            // bt_cmd = ',';                                       // TODO :BAKA
-            // break;                                       // TODO :BAKA
-        // case 'l':                                       // TODO :BAKA
-            // bt_cmd = 'l';                                       // TODO :BAKA
-            // break;                                       // TODO :BAKA
-        // case '.':                                       // TODO :BAKA
-            // bt_cmd = '.';                                       // TODO :BAKA
-            // break;                                       // TODO :BAKA
-
         case 't':
             bt_cmd = 't';
             break;
